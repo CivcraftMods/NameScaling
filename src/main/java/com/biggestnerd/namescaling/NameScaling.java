d73b7b351789d8fb2c7997dff2e9348e5675215d
teal <tealnerd@gmail.com>
2016-05-13 05:17:51 -0500

    Update to 1.9

--- src/main/java/com/biggestnerd/namescaling/NameScaling.java
+++ src/main/java/com/biggestnerd/namescaling/NameScaling.java
@@ -1,19 +1,23 @@
 package com.biggestnerd.namescaling;
 
-import java.awt.Color;
-
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.GL11;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityOtherPlayerMP;
 import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.GlStateManager.DestFactor;
+import net.minecraft.client.renderer.GlStateManager.SourceFactor;
 import net.minecraft.client.renderer.Tessellator;
-import net.minecraft.client.renderer.WorldRenderer;
+import net.minecraft.client.renderer.VertexBuffer;
 import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.client.renderer.texture.ITextureObject;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.client.settings.KeyBinding;
-import net.minecraft.util.ChatComponentText;
-import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.client.event.RenderLivingEvent;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.common.MinecraftForge;
@@ -25,7 +29,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent.KeyInputEvent;
 
-@Mod(modid="namescaling", name="Name Scaling", version="v1.0")
+@Mod(modid="namescaling", name="Name Scaling", version="1.0")
 public class NameScaling {
 
 	private Minecraft mc;
@@ -45,7 +49,7 @@ public class NameScaling {
 		for(Object o : mc.theWorld.loadedEntityList) {
 			if(o instanceof EntityOtherPlayerMP) {
 				EntityOtherPlayerMP player = (EntityOtherPlayerMP) o;
-				renderNametag(player, event.partialTicks);
+				renderNametag(player, event.getPartialTicks());
 			}
 		}
 	}
@@ -54,13 +58,13 @@ public class NameScaling {
 	public void onKeyInput(KeyInputEvent event) {
 		if(toggle.isKeyDown()) {
 			enabled = !enabled;
-			mc.thePlayer.addChatMessage(new ChatComponentText(EnumChatFormatting.DARK_AQUA + "[NameScaling]" + EnumChatFormatting.GRAY + "Scaling " + (enabled ? "enabled" : "disabled")));
+			mc.thePlayer.addChatMessage(new TextComponentString(TextFormatting.DARK_AQUA + "[NameScaling]" + TextFormatting.GRAY + "Name scaling " + (enabled ? "enabled" : "disabled")));
 		}
 	}
 	
 	@SubscribeEvent
 	public void onRenderLiving(RenderLivingEvent.Specials.Pre event) {
-		if(event.entity instanceof EntityOtherPlayerMP) {
+		if(event.getEntity() instanceof EntityOtherPlayerMP) {
 			event.setCanceled(true);
 		}
 	}
@@ -68,7 +72,7 @@ public class NameScaling {
 	private void renderNametag(EntityOtherPlayerMP player, float partialTickTime) {
 		String name = player.getName();
 		int dist = (int) player.getDistanceToEntity(mc.thePlayer);
-		name += " (" + dist + ")";
+		name += " (" + dist + "m)";
 		
 		RenderManager rm = mc.getRenderManager();
 		FontRenderer fr = rm.getFontRenderer();
@@ -79,7 +83,7 @@ public class NameScaling {
 				* partialTickTime);
 		float renderPosZ = (float) (mc.thePlayer.lastTickPosZ + (mc.thePlayer.posZ - mc.thePlayer.lastTickPosZ)
 				* partialTickTime);
-
+		
 		float dx = (float)player.posX - renderPosX;
 		float dy = (float)(player.posY + (player.isSneaking() ? 1.9 : 2.2)) - renderPosY;
 		float dz = (float)player.posZ - renderPosZ;
@@ -87,37 +91,37 @@ public class NameScaling {
 		
 		float scale = (float) (-0.09+1/((1+Math.pow(1.3, 10-distance/20))*.6));
 		
-		GL11.glPushMatrix();
-		GL11.glTranslatef(dx, dy, dz);
-		GL11.glRotatef(-rm.playerViewY, 0.0F, 1.0F, 0.0F);
-		GL11.glRotatef(rm.playerViewX, 1.0F, 0.0F, 0.0F);
-		GL11.glScalef(-scale, -scale, scale);
-		GL11.glDisable(2896);
-		GL11.glDepthMask(false);
-		GL11.glDisable(2929);
-		GL11.glEnable(3042);
-		GL11.glBlendFunc(770, 771);
-		
+		GlStateManager.pushMatrix();
+		GlStateManager.translate(dx, dy, dz);
+		GlStateManager.glNormal3f(0.0F, 1.0F, 0.0F);
+		GlStateManager.rotate(-rm.playerViewY, 0.0F, 1.0F, 0.0F);
+		GlStateManager.rotate(rm.playerViewX, 1.0F, 0.0F, 0.0F);
+		GlStateManager.scale(-scale, -scale, scale);
+		GlStateManager.disableLighting();
+		GlStateManager.depthMask(false);
+		GlStateManager.disableDepth();
+		GlStateManager.enableBlend();
+		GlStateManager.tryBlendFuncSeparate(SourceFactor.SRC_ALPHA, DestFactor.ONE_MINUS_SRC_ALPHA, SourceFactor.ONE, DestFactor.ZERO);
 		int textWidth = fr.getStringWidth(name);
 		int lineHeight = fr.FONT_HEIGHT;
 		Tessellator t = Tessellator.getInstance();
-		WorldRenderer wr = t.getWorldRenderer();
-		GL11.glDisable(GL11.GL_TEXTURE_2D);
-		wr.startDrawingQuads();
+		VertexBuffer vb = t.getBuffer();
+		GlStateManager.disableTexture2D();
+		vb.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_COLOR);
 		int stringMiddle = textWidth / 2;
-		wr.setColorRGBA_F(0.0F, 0.0F, 0.0F, 0.25F);
-		wr.addVertex(-stringMiddle - 1, -1.0D, 0.0D);
-		wr.addVertex(-stringMiddle - 1, lineHeight, 0.0D);
-		wr.addVertex(stringMiddle + 1, lineHeight, 0.0D);
-		wr.addVertex(stringMiddle + 1, -1, 0.0D);
+		vb.pos(-stringMiddle - 1, -1.0D, 0.0D).color(0.0F, 0.0F, 0.0F, 0.3F).endVertex();
+		vb.pos(-stringMiddle - 1, lineHeight, 0.0D).color(0.0F, 0.0F, 0.0F, 0.3F).endVertex();
+		vb.pos(stringMiddle + 1, lineHeight, 0.0D).color(0.0F, 0.0F, 0.0F, 0.3F).endVertex();
+		vb.pos(stringMiddle + 1, -1, 0.0D).color(0.0F, 0.0F, 0.0F, 0.3F).endVertex();
 		t.draw();
-		GL11.glEnable(GL11.GL_TEXTURE_2D);
-		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-		int color = player.isSneaking() ? Color.GREEN.getRGB() : Color.WHITE.getRGB();
+		GlStateManager.enableTexture2D();
+		int color = player.isSneaking() ? -16711936 : 2852543;
+		GlStateManager.enableDepth();
+		GlStateManager.depthMask(true);
 		fr.drawString(name, -textWidth / 2, 0, color);
-		GL11.glDepthMask(true);
-		GL11.glEnable(2929);
-		GL11.glPopMatrix();
-		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+		GlStateManager.enableLighting();
+		GlStateManager.disableBlend();
+		GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+		GlStateManager.popMatrix();
 	}
 }
